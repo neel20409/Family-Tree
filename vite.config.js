86e6c301f9a784278ac40a536fb8845e996fef67
Neel <bhattneel2004@gmail.com>
2025-06-06 12:13:18 +0530

    fix: set correct base path '/familytree/' for Vite deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/familytree/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
